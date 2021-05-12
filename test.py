1d8bb2dedb31553734ff4fc503fc0dc406a64532
LeeHyunWoo <silving0918@naver.com>
2021-05-12 17:22:13 +0900

    3053

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-x,y,z=1,1,1
-while True:
-    x,y,z=map(int,input().split())
-    if x+y+z==0: break
-    if x**2+y**2==z**2 or x**2+z**2==y**2 or y**2+z**2==x**2:
-        print('right')
-    else:
-        print('wrong')
+import math
+r = int(input())
+print(format(math.pi*(r**2),".6f"))
+print(format(float(2*(r**2)),".6f"))

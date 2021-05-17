0b2fb3584b69530caeb8385616ec1149439abffe
LeeHyunWoo <silving0918@naver.com>
2021-05-17 20:41:04 +0900

    counting_sort

--- test.py
+++ test.py
@@ -1,32 +1,17 @@
-def merge_sort(list):
-    if len(list)<=1:
-        return list
-    else:
-        mid = len(list)//2
-        left = list[:mid]
-        right = list[mid:]
-        left = merge_sort(left)
-        right = merge_sort(right)
-        return merge(left,right)
-
-def merge(left,right):
-    result = []
-    i = j =0
-    while i<len(left) and j <len(right):
-        if left[i]>right[j]:
-            result.append(right[j])
-            j+=1
-        else:
-            result.append(left[i])
-            i+=1
-    if i<len(left):
-        result += left[i:len(left)]
-    else:
-        result += right[j: len(right)]
-    return result
-
+def counting_sort(arr, max):
+    c_arr = [0]*(max+1)
+    for i in arr:
+        c_arr[i] += 1
+    for i in range(max):
+        c_arr[i+1] += c_arr[i]
+    o_arr = [-1]*len(arr)
+    for i in arr:
+        o_arr[c_arr[i] -1] = i
+        c_arr[i] -= 1
+    return o_arr
 a=[]
-for _ in range(int(input())):
-   a.append(int(input()))
-
-print('\n'.join(list(map(str, merge_sort(a)))))
+for i in range(int(input())):
+    a.append(int(input()))
+    
+result = counting_sort(a,max(a))
+print('\n'.join(map(str,result)))

9857118dd237f345b5cf26fb23731252cb24eca4
LeeHyunWoo <silving0918@naver.com>
2021-05-20 12:06:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-def counting_sort(arr, max):
-    c_arr = [0]*(max+1)
+def counting_sort(arr):
+    c_arr = [0]*(max(arr))
     for i in arr:
-        c_arr[i] += 1
-    for i in range(max):
-        c_arr[i+1] += c_arr[i]
+        c_arr[i-1] += 1
+    for i in range(max(arr)):
+        c_arr[i] += c_arr[i-1]
     o_arr = [-1]*len(arr)
     for i in arr:
-        o_arr[c_arr[i] -1] = i
-        c_arr[i] -= 1
+        o_arr[c_arr[i-1] -1] = i
+        c_arr[i-1] -= 1
     return o_arr
 a=[]
 for i in range(int(input())):
     a.append(int(input()))
     
-result = counting_sort(a,max(a))
+result = counting_sort(a)
 print('\n'.join(map(str,result)))
